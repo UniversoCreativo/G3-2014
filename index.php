9de3a23926c9826e943cbfa08b488339a635730c
ezequias24 <eze_mcmashi@hotmail.com.ar>
2014-08-21 16:50:35 -0300

    aca esta gordo molesto

--- index.php
+++ index.php
@@ -1,36 +1,48 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
 <link rel="stylesheet" href="css/estilo.css" type="text/css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Documento sin título</title>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 </head>
 
 <body>
 <div class="contenedora">
 <div class="contenedora2">
     <div id="bloquea">
-        <div id="columnah"></div>
-        <div id="columnav"></div>
+    
+        <div id="columnah">URL PANEL</div>
+ 		<div id="columnav">
+        <img src="imagenes/rulo1.png" />
+        </div>
     </div>
     
     <div id="bloqueb">
-        <div id="caja1">
-        <div id="molde1"></div>
-        <div id="molde2"></div>
-		</div>
-        
-        <div id="caja2">
-        <div id="molde3"></div>
-        <div id="molde4"></div>
-    	</div>
-    	
-        <div id="caja3">
-        <div id="molde5"></div>
-        <div id="molde6"></div>
-    	</div>
-    </div>
+   
+   <form id="f_acceso" name="f_acceso" action="acceso.php" method="POST">
+
+<div class="caja1">
+	<label for="textfield">USUARIO:</label>
+	<input type="text" name="f_usuario" id="f_usuario">
+    <br />
+    <div class="imagen1">
+ <img src="imagenes/rulo22.png" width="231"class="caja1" /></div>
 </div>
+<br>
+	<div class="caja2">
+<label for="textfield">CONTRASEÑA:</label>
+<input type="text" name="f_contra" id="f_contra">
+<br />
+ <img src="imagenes/rulo22.png" width="231"class="caja2" /></div>
+<br>
+	<div class="caja3">
+<input type="submit" name="f_boton" id="f_boton" value="INICIAR">
+</div>
+</form>
+</div>
+
 </div>
 </body>
 </html>
---
+++ jodiendo/Untitled-1.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div class="contenedora">
+	<div class="caja1"> </div>
+ 	<div class="caja2"> </div>
+    <div class="caja3"> </div>
+    <div class="face"> </div>
+    <div class="twt"> </div>
+    <div class="google"> </div>
+</div>
+    
+     
+</body>
+</html>
